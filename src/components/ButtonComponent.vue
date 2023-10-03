2e98d9c361fa1213f97412a84285b3039db164ab
Luciano Gomes <lucianogvda02@gmail.com>
2023-10-03 16:47:25 -0300

    feat(src/components/ButtonComponent.vue): inmplements ButtonComponent

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <button
+        class="button"
+        :style="button.styles"
+    >
+      {{ button.title }}
+    </button>
+</template>
+
+<script>
+export default {
+  name: "ButtonComponent",
+  props: {
+    button: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.button {
+  margin-top: 1px;
+  border: none;
+  color: #ffffff;
+  background-color: #000;
+  border-radius: 41px;
+}
+
+.button:hover {
+  opacity: 80%;
+}
+</style>
