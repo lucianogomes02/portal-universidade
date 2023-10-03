d4eacbc6f058957c9c3e83f1a4ee739492fd36ce
Luciano Gomes <lucianogvda02@gmail.com>
2023-10-03 02:26:52 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+}
+</style>
