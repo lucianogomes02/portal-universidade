3bb62ab398dfa7d862cd6d06848da8449c13cfd8
Luciano Gomes <lucianogvda02@gmail.com>
2023-10-03 20:30:24 -0300

    feat(src/store.js): implements store

---
+++ src/store.js
@@ -0,0 +1,26 @@
+import { createStore } from 'vuex';
+
+const state = {
+    user: null
+};
+
+const store = createStore({
+    state,
+    getters: {
+        user: (state) => {
+            return state.user;
+        }
+    },
+    actions: {
+        setUser(context, user){
+            context.commit("user");
+        }
+    },
+    mutations: {
+        user(state, user) {
+            state.user = user;
+        }
+    },
+});
+
+export default store;
